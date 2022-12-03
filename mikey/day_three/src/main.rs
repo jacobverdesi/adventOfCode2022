b898bb4dc8d57bcb5410282d6247f18253df7c81
Mikey Hoy <mjh@mjhoy.com>
2022-12-03 09:32:32 -0500

    cleanup

--- mikey/day_three/src/main.rs
+++ mikey/day_three/src/main.rs
@@ -13,16 +13,22 @@ fn priority(c: char) -> i64 {
 fn main() -> std::io::Result<()> {
     let path = common::get_first_arg("usage: day_three [path]")?;
 
+    // since i'm allocating the whole vec in part two to use `chunks`, just do it here :(
+    // other options: use a crate like itertools, use nightly for `array_chunks` iter method
+    let lines_iter = common::open_lines(&path)?;
+    let lines = lines_iter
+        .map(|line| line.expect("failed reading line"))
+        .collect::<Vec<String>>();
+
     // Part one
     {
-        let lines = common::open_lines(&path)?;
         let sum: i64 = lines
+            .iter()
             .map(|line| {
-                let line = line.expect("failed reading line");
                 let chars = line.chars().collect::<Vec<_>>();
                 let (compartment_a, compartment_b) = chars.split_at(chars.len() / 2);
                 let compartment_a_set: BTreeSet<char> =
-                    Vec::from(compartment_a).into_iter().collect();
+                    Vec::from(compartment_a).into_iter().collect(); // are these allocations necessary?
                 let compartment_b_set: BTreeSet<char> =
                     Vec::from(compartment_b).into_iter().collect();
                 compartment_a_set
@@ -37,26 +43,19 @@ fn main() -> std::io::Result<()> {
 
     // Part two
     {
-        let lines = common::open_lines(&path)?;
         let sum: i64 = lines
-            .collect::<Vec<_>>()
             .chunks(3) // unstable has `array_chunks` which would avoid the Vec allocation
             .map(|elf_group| {
                 elf_group
                     .iter()
-                    .map(|line| {
-                        line.as_ref() // move from a reference to a result of T -> result of a reference of T
-                            .expect("error reading line")
-                            .chars()
-                            .collect::<BTreeSet<char>>()
-                    })
+                    .map(|line| line.chars().collect::<BTreeSet<char>>())
                     .reduce(|accum, set| {
                         accum
                             .intersection(&set)
                             .cloned()
                             .collect::<BTreeSet<char>>()
                     })
-                    .expect("expected element for reduce")
+                    .expect("expected at least one element for reduce")
                     .iter()
                     .map(|x| priority(*x) as i64)
                     .sum::<i64>()
