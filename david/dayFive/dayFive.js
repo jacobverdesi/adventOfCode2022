dbd0c500b10cf2ddf724a8475d7a9d6cb7ae9431
DavidHarris <dharr152@github.com>
2022-12-05 13:49:18 -0500

    dave/added code comments

--- david/dayFive/dayFive.js
+++ david/dayFive/dayFive.js
@@ -1,5 +1,6 @@
 const co = require("../common");
 
+// get the stack elements from input
 function parseStackInputs(row) {
   let stackRow = [];
   for (let i = 1; i < row[0].length; i += 4) {
@@ -8,40 +9,43 @@ function parseStackInputs(row) {
   return stackRow;
 }
 
+// remove non-numerics and empty values from move input
 function parseMoveInput(move) {
-    const moveSentence = move.split(' ')
-    return moveSentence.map(i => i.replace(/\D/g,'')).filter(i => i != '')
+  const moveSentence = move.split(" ");
+  return moveSentence.map((i) => i.replace(/\D/g, "")).filter((i) => i != "");
 }
 
+// move elements from one stack to another (Question 1)
 function modifyStacksUsingPop(move, stacks) {
-  const itemsToMove = parseInt(move[0], 10)
-  const moveFrom = parseInt(move[1], 10) -1
-  const moveTo = parseInt(move[2], 10) -1
-    for (let i = 0; i < itemsToMove; i++) {
-        stacks[moveTo].push(stacks[moveFrom].pop());
-    }
+  const itemsToMove = parseInt(move[0], 10);
+  const moveFrom = parseInt(move[1], 10) - 1;
+  const moveTo = parseInt(move[2], 10) - 1;
+  for (let i = 0; i < itemsToMove; i++) {
+    stacks[moveTo].push(stacks[moveFrom].pop());
+  }
 }
 
+// move elements from one stack to intermediate stack to final stack (Question 2)
 function modifyStacksUsingLoader(move, stacks) {
-  const itemsToMove = parseInt(move[0], 10)
-  const moveFrom = parseInt(move[1], 10) - 1
-  const moveTo = parseInt(move[2], 10) - 1
-  let loader = []
-    for (let i = 0; i < itemsToMove; i++) {
-        loader.push(stacks[moveFrom].pop());
-    }
-    for (let i = 0; i < itemsToMove; i ++) {
-        stacks[moveTo].push(loader.pop())
-    }
+  const itemsToMove = parseInt(move[0], 10);
+  const moveFrom = parseInt(move[1], 10) - 1;
+  const moveTo = parseInt(move[2], 10) - 1;
+  let loader = [];
+  for (let i = 0; i < itemsToMove; i++) {
+    loader.push(stacks[moveFrom].pop());
+  }
+  for (let i = 0; i < itemsToMove; i++) {
+    stacks[moveTo].push(loader.pop());
+  }
 }
 
-//
+// process all element moves and read last element from each stack
 function getLastStackItems(textArray, moveLogic) {
   let result = [];
   let stacks = [];
   let moves = [];
 
-  // initialize blank lists for stacks
+  // initialize blank lists for each stack
   for (let i = 1; i < textArray[0].length; i += 4) {
     stacks.push([]);
   }
@@ -50,7 +54,7 @@ function getLastStackItems(textArray, moveLogic) {
   for (let i in textArray) {
     const row = textArray[i];
     if (row) {
-      // if row includes stack inputs
+      // if row includes stack inputs parse the stack
       if (row.includes("[")) {
         let singleStack = parseStackInputs(row.split());
         for (let i in singleStack) {
@@ -58,19 +62,22 @@ function getLastStackItems(textArray, moveLogic) {
             stacks[i].push(singleStack[i]);
           }
         }
+        // if row includes move instruction process moves
       } else if (row.includes("move")) {
-        moves.push(parseMoveInput(row)); // remove non numeric inputs
+        moves.push(parseMoveInput(row));
       }
     }
   }
+  // reverse all stacks to first element read in as the last element in the stack
   stacks.map((i) => i.reverse());
   for (let i in moves) {
+    // process all moves according to move logic function
     moveLogic(moves[i], stacks);
   }
 
   // add last element from all stacks into a final...stack
   for (let i in stacks) {
-    result.push(stacks[i][stacks[i].length-1])
+    result.push(stacks[i][stacks[i].length - 1]);
   }
   return result.join("");
 }
@@ -78,4 +85,3 @@ function getLastStackItems(textArray, moveLogic) {
 const input = co.textFileToArray("./dayFive.txt");
 console.log(getLastStackItems(input, modifyStacksUsingPop));
 console.log(getLastStackItems(input, modifyStacksUsingLoader));
-
