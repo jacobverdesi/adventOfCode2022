f311ea8d1fbc4c203e10360e33ab761fc5d6eb96
Mikey Hoy <mjh@mjhoy.com>
2022-12-05 17:38:50 -0500

    break into functions

--- mikey/day_five/src/main.rs
+++ mikey/day_five/src/main.rs
@@ -2,54 +2,48 @@ use std::collections::{HashMap, VecDeque};
 use std::fs::File;
 use std::io::{BufReader, Lines};
 
-fn calculate(lines: Lines<BufReader<File>>, part_two: bool) {
-    // BACK is bottom, FRONT is top.
-    let mut stacks: HashMap<usize, VecDeque<char>> = HashMap::new();
-    let mut is_moving: bool = false;
+type Stacks = HashMap<usize, VecDeque<char>>;
 
-    for line in lines {
-        let line = line.as_ref().expect("failed reading line");
-
-        if !is_moving {
-            let mut any_chars = false;
-            let chars = line
-                .chars()
-                .enumerate()
-                .filter(|(i, _)| ((*i as i32) - 1) % 4 == 0)
-                .map(|(_, c)| c);
-            for (i, c) in chars.enumerate() {
-                if char::is_uppercase(c) {
-                    any_chars = true;
-                    let v = stacks.entry(i).or_insert_with(VecDeque::new);
-                    v.push_back(c);
-                }
-            }
-            is_moving = !any_chars;
-        } else {
-            match line
-                .split(' ')
-                .filter_map(|s| s.parse::<usize>().ok())
-                .collect::<Vec<usize>>()
-                .as_slice()
-            {
-                [num, from, to] => {
-                    let from_stack = stacks.get_mut(&(from - 1)).unwrap();
-                    let mut froms = (0..*num)
-                        .map(|_| from_stack.pop_front().unwrap())
-                        .collect::<Vec<_>>();
-                    if part_two {
-                        froms.reverse();
-                    }
-                    let to_stack = stacks.get_mut(&(to - 1)).unwrap();
-                    for from in froms {
-                        to_stack.push_front(from);
-                    }
-                }
-                _ => continue,
-            }
+fn process_stack_line(stacks: &mut Stacks, line: &str) -> bool {
+    let mut any_chars = false;
+    let chars = line
+        .chars()
+        .enumerate()
+        .filter(|(i, _)| ((*i as i32) - 1) % 4 == 0)
+        .map(|(_, c)| c);
+    for (i, c) in chars.enumerate() {
+        if char::is_uppercase(c) {
+            any_chars = true;
+            let v = stacks.entry(i).or_insert_with(VecDeque::new);
+            v.push_back(c);
         }
     }
 
+    any_chars
+}
+
+fn process_instruction_line(stacks: &mut Stacks, line: &str, part_two: bool) {
+    if let [num, from, to] = line
+        .split(' ')
+        .filter_map(|s| s.parse::<usize>().ok())
+        .collect::<Vec<usize>>()
+        .as_slice()
+    {
+        let from_stack = stacks.get_mut(&(from - 1)).unwrap();
+        let mut froms = (0..*num)
+            .map(|_| from_stack.pop_front().unwrap())
+            .collect::<Vec<_>>();
+        if part_two {
+            froms.reverse();
+        }
+        let to_stack = stacks.get_mut(&(to - 1)).unwrap();
+        for from in froms {
+            to_stack.push_front(from);
+        }
+    }
+}
+
+fn print_answers(stacks: &Stacks, part_two: bool) {
     let mut answers = stacks
         .iter()
         .map(|(k, stack)| (k, stack.front()))
@@ -64,6 +58,24 @@ fn calculate(lines: Lines<BufReader<File>>, part_two: bool) {
         print!("{}", answer.unwrap());
     }
     println!();
+}
+
+fn calculate(lines: Lines<BufReader<File>>, part_two: bool) {
+    // BACK is bottom, FRONT is top.
+    let mut stacks: Stacks = HashMap::new();
+    let mut is_moving: bool = false;
+
+    for line in lines {
+        let line = line.as_ref().expect("failed reading line");
+
+        if !is_moving {
+            is_moving = !process_stack_line(&mut stacks, line);
+        } else {
+            process_instruction_line(&mut stacks, line, part_two);
+        }
+    }
+
+    print_answers(&stacks, part_two);
 }
 
 fn main() -> std::io::Result<()> {
